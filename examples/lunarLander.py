343211595dfe8faf30928aef1c5fbbd240c8fe9d
Inexperienced Me <inexperiencedme@protonmail.com>
2026-02-09 13:42:05 +0100

    self-contained examples

--- examples/lunarLander.py
+++ examples/lunarLander.py
@@ -4,11 +4,27 @@ import argparse
 import copy
 import multiprocessing
 import signal
-from utils.environmentUtils import CleanLunarLander
+from gymnasium.spaces.utils import flatdim
 from SimpleNEAT.NEAT import NEAT
-        
-workerEnvironment = None
 
+class CleanLunarLander(gym.Wrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.observationSize = flatdim(self.env.observation_space)
+        self.actionSize      = flatdim(self.env.action_space)
+
+
+    def step(self, action):
+        observations, reward, terminated, truncated, info = self.env.step(action)
+        done = terminated or truncated
+        return observations, reward, done
+
+    def reset(self, seed=None):
+        observations, info = self.env.reset(seed=seed)
+        return observations
+
+
+workerEnvironment = None
 def initializeWorker():
     signal.signal(signal.SIGINT, signal.SIG_IGN) # Ignore C-c, parent will handle it
 
