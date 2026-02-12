3dba1c4f46c2e62eae32a4aef9e2ddc070d4fbf4
Inexperienced Me <inexperiencedme@protonmail.com>
2026-02-12 14:43:42 +0100

    somewhat cleaner mario, but stuck on first gen due to never ending episodes

--- examples/superMarioWorld.py
+++ examples/superMarioWorld.py
@@ -1,46 +1,27 @@
 import stable_retro as retro
 import gymnasium as gym
+from gymnasium.wrappers import ResizeObservation, GrayscaleObservation
 from SimpleNEAT.utils import loadConfig
 from SimpleNEAT.trainer import runEvolution
 from SimpleNEAT.showcaseOrganism import showcaseOrganism
 
 class CleanMario(gym.Wrapper):
     def __init__(self, env):
-        self.env = env
-        self.observationSize = 16 * 14 
-        self.actionSize = 12 
-        
-        self.time_since_last_reward = 0
-        self.current_total_reward = 0.0
+        self.env = GrayscaleObservation(ResizeObservation(env, (16, 16)))
+        self.observationSize = 16 * 16
+        self.actionSize = 12
 
-    def process_obs(self, obs):
-        downsampled = obs[::16, ::16, :3]
-        gray = (downsampled[:,:,0] * 0.2989 + downsampled[:,:,1] * 0.5870 + downsampled[:,:,2] * 0.1140)
-        return gray.flatten() / 255.0
+    def processObservation(self, obs):
+        return obs.flatten() / 255.0
 
     def reset(self, seed=None):
-        state, info = self.env.reset(seed=seed)
-            
-        self.time_since_last_reward = 0
-        self.current_total_reward = 0.0
-        return self.process_obs(state)
+        obs, info = self.env.reset(seed=seed)
+        return self.processObservation(obs)
 
     def step(self, action):
-        binary_action = [1 if x > 0 else 0 for x in action]
-        obs, reward, terminated, truncated, info = self.env.step(binary_action)
-        
-        self.current_total_reward += reward
-
-        if reward > 0:
-            self.time_since_last_reward = 0
-        else:
-            self.time_since_last_reward += 1
-            
-        is_stagnant = self.time_since_last_reward > 1000
-        
-        done = terminated or truncated or is_stagnant
-        return self.process_obs(obs), reward, done
-
+        obs, reward, terminated, truncated, info = self.env.step([1 if x > 0 else 0 for x in action])
+        return self.processObservation(obs), reward, terminated or truncated
+    
 def environmentMaker(render_mode=None):
     return CleanMario(retro.make("SuperMarioWorld-Snes-v0", state="DonutPlains1", render_mode=render_mode))
 
