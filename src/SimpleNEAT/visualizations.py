ae7e64539f261f9bc5606503f4ae8c07b35f4e3a
Inexperienced Me <inexperiencedme@protonmail.com>
2026-02-15 14:23:40 +0100

    even better names

--- src/SimpleNEAT/visualizations.py
+++ src/SimpleNEAT/visualizations.py
@@ -23,21 +23,21 @@ def createVisualization(observation, canvasStartPoint, canvasEndPoint, gridColor
         observationRGBA01 = np.stack([observation01, observation01, observation01, np.ones_like(observation01)], axis=-1)
     observationRGBA = observationRGBA01
 
-    canvasHeightRequested = abs(canvasEndPoint[1] - canvasStartPoint[1])
-    cellSize = (canvasHeightRequested - gridThickness * (rows + 1)) // rows
+    canvasHeight = abs(canvasEndPoint[1] - canvasStartPoint[1])
+    cellSize = (canvasHeight - gridThickness * (rows + 1)) // rows
 
-    gridHeight = (rows * cellSize) + ((rows + 1) * gridThickness)
-    gridWidth  = (cols * cellSize) + ((cols + 1) * gridThickness)
+    observationVisualizationHeight = (rows * cellSize) + ((rows + 1) * gridThickness)
+    observationVisualizationWidth  = (cols * cellSize) + ((cols + 1) * gridThickness)
     
-    observationGrid = np.full((gridHeight, gridWidth, 4), gridColor)
+    observationVisualization = np.full((observationVisualizationHeight, observationVisualizationWidth, 4), gridColor)
 
     for row in range(rows):
         y_top = gridThickness + row * (cellSize + gridThickness)
         for column in range(cols):
             x_left = gridThickness + column * (cellSize + gridThickness)
-            observationGrid[y_top:y_top + cellSize, x_left:x_left + cellSize] = observationRGBA[row, column]
+            observationVisualization[y_top:y_top + cellSize, x_left:x_left + cellSize] = observationRGBA[row, column]
 
-    return (observationGrid*255).astype(np.uint8)
+    return (observationVisualization*255).astype(np.uint8)
 
 def embedForegroundOnFrame(foreground, frame, position, globalAlpha=1.0):
     offsetX, offsetY = position
